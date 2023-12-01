2a1224beec217846910cac380d96f77f80922df0
realdump <a.majeed17@outlook.com>
2023-12-01 12:21:44 +0530

    update

---
+++ Camera Classifier/app.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import simpledialog
+import cv2 as cv
+import os
+import PIL.Image, PIL.ImageTk
+import camera
+
+class App:
+    def __init__(self, window=tk.Tk(), window_title="Camera Classifier"):
+        
+        self.window= window
+        self.window_title = window_title
+        self.counters = [1,1]
+
+        #self.model = 
+
+        self.auto_predict = False
+
+        self.camera = camera.Camera()
+
+        #self
+
+        self.delay = 15
+
+        #self.update()
